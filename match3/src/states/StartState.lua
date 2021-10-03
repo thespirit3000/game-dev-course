eaf0c9d6e004f2d0ea40c8020b501a6f71cd571d
Dzianis Bogdan <bogdan.den@gmail.com>
2021-10-03 23:08:20 +0300

    feat: animation start screen

--- match3/src/states/StartState.lua
+++ match3/src/states/StartState.lua
@@ -14,12 +14,18 @@ function StartState:init()
     self.leterTable = {
         {'S', -108}, {'P', -64}, {'A', -28}, {'C', 2}, {'E', 40}, {'3', 112}
     }
+    self.backgroundY = -64
+    self.transitionAlpha = 0
     self.colorTimer = 0
+    TIMER.tween(3, {[self] = {backgroundY = VIRTUAL_HEIGHT / 2 - 16}})
 end
 
 function StartState:update(dt)
+    TIMER.update(dt)
     if love.keyboard.wasPressed("enter") or love.keyboard.wasPressed("return") then
-        gStateMachine:change("serve", {player = Ship(VIRTUAL_WIDTH / 2 - 8)})
+        TIMER.tween(1, {[self] = {transitionAlpha = 1}}):finish(function()
+            gStateMachine:change("serve", {player = Ship(VIRTUAL_WIDTH / 2 - 8)})
+        end)
     end
     self.colorTimer = self.colorTimer + dt
     if self.colorTimer > 0.5 then
@@ -27,16 +33,17 @@ function StartState:update(dt)
         self.colors[0] = self.colors[6]
         for i = 6, 1, -1 do self.colors[i] = self.colors[i - 1] end
     end
-
 end
 
-function StartState:render() self:drawTitle(VIRTUAL_HEIGHT / 2 - 16) end
+function StartState:render() self:drawTitle() end
 
 function StartState:drawTitle(y)
     love.graphics.setFont(gFonts["large"])
     for i = 1, 6 do
         love.graphics.setColor(self.colors[i])
-        love.graphics.printf(self.leterTable[i][1], 0, y,
+        love.graphics.printf(self.leterTable[i][1], 0, self.backgroundY,
                              VIRTUAL_WIDTH + self.leterTable[i][2], 'center')
     end
+    love.graphics.setColor(0, 0, 0, self.transitionAlpha)
+    love.graphics.rectangle('fill', 0, 0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT)
 end
